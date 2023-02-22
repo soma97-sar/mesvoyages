cf70aac319116a864e9de521f15c580aa52e5ecd
soma97-sar <BEN BAHA@sokal.lan>
2023-02-22 01:28:09 +0100

    Phase14: ORM(ajout d'un enregistrement).

--- src/Controller/admin/AdminVoyagesController.php
+++ src/Controller/admin/AdminVoyagesController.php
@@ -49,6 +49,24 @@ class AdminVoyagesController extends AbstractController{
         ]);
     }
     /**
+     * @Route("/admin/ajout", name="admin.voyage.ajout")
+     * @param Request $request
+     * @return Response
+     */
+    public function ajout( Request $request): Response {
+        $visite=new Visite();
+        $formVisite=$this->createForm(VisiteType::class,$visite);
+        $formVisite->handleRequest($request);
+        if($formVisite->isSubmitted() && $formVisite->isValid()){
+            $this->repository->add($visite, true);
+            return $this->redirectToRoute('admin.voyages');
+        }
+        return $this->render("admin/admin.voyage.ajout.html.twig",[
+            'visite'=>$visite,
+            'formvisite'=>$formVisite->createView()    
+        ]);
+    }
+    /**
      * @Route("/admin/suppr/{id}", name="admin.voyage.suppr")
      * @param Visite $visite
      * @return Response
--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -4,9 +4,11 @@ namespace App\Form;
 
 use App\Entity\Visite;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class VisiteType extends AbstractType
 {
@@ -15,17 +17,19 @@ class VisiteType extends AbstractType
         $builder
             ->add('ville')
             ->add('pays')
-            ->add('datecreation', null, [
+            ->add('datecreation', DateType::class, [
                 'widget'=>'single_text',
+                'data'=> isset($options['data']) &&
+                    $options['data']->getDateCreation() !=null ? $options['data']->getDateCreation() : new DateTime('now'),
                 'label'=>'date'
             ])
             ->add('note')
             ->add('avis')
             ->add('tempmin',null, [
-                'label'=>'° min'
+                'label'=>'t° min'
             ])
             ->add('tempmax', null, [
-                'label'=>'°max'
+                'label'=>'t°max'
             ])
             ->add('submit',SubmitType::class, [
                 'label'=>'Enregistrer'
