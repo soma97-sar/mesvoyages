dbcba8f30cae0fddf8a4e9c64dd942c9de24f2d8
soma97-sar <BEN BAHA@sokal.lan>
2023-02-28 11:46:06 +0100

    Phase19:gestion des images.

--- migrations/Version20230223204756.php
+++ migrations/Version20230223204756.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230220095357 extends AbstractMigration
+final class Version20230223204756 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20230220095357 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE visite ADD tempmin INT DEFAULT NULL, ADD tempmax INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE visite ADD image_name VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE visite DROP tempmin, DROP tempmax');
+        $this->addSql('ALTER TABLE visite DROP image_name');
     }
 }
--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -3,14 +3,45 @@
 namespace App\Entity;
 
 use App\Repository\VisiteRepository;
+use DateTime;
+use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
+
+  
 #[ORM\Entity(repositoryClass: VisiteRepository::class)]
+  
+ 
+/**
+ * @ORM\Entity(repositoryClass="VisiteRepository")
+ * @Vich\Uploadable
+ */
 class Visite
 {
+     /**
+     * NOTE:This is not a mapped field of entity metadata;just a simple property.
+     * @Vich\UploadableField(mapping="visites", fileNameProperty="imageName")
+      * @Assert\Image(mimeTypes="image/jpeg")
+     * @var File|null
+     */
+    private $imageFile;
+     
+    /**
+     *
+     * 
+     * @var string|null
+     */
+    #[ORM\Column( length:255, nullable:true)] 
+     
+    private ?string $imageName=null;
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -23,7 +54,7 @@ class Visite
     private ?string $pays = null;
 
     #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
-    private ?\DateTimeInterface $datecreation = null;
+    private ?DateTimeInterface $datecreation = null;
 
     #[ORM\Column(nullable: true)]
     private ?int $note = null;
@@ -39,6 +70,9 @@ class Visite
 
     #[ORM\ManyToMany(targetEntity: Environnement::class)]
     private Collection $environnements;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $updated_at = null;
 
     public function __construct()
     {
@@ -74,12 +108,12 @@ class Visite
         return $this;
     }
 
-    public function getDatecreation(): ?\DateTimeInterface
+    public function getDatecreation(): ?DateTimeInterface
     {
         return $this->datecreation;
     }
 
-    public function setDatecreation(?\DateTimeInterface $datecreation): self
+    public function setDatecreation(?DateTimeInterface $datecreation): self
     {
         $this->datecreation = $datecreation;
 
@@ -164,5 +198,61 @@ class Visite
         $this->environnements->removeElement($environnement);
 
         return $this;
+    }
+   
+    function getImageFile(): ?File{
+       
+           return $this->imageFile;
+    }
+    function getImageName(): ?string{
+        return $this->imageName;
+    }
+    function setImageFile(File $imageFile): self{
+        $this->imageFile= $imageFile;
+        if($this->imageFile instanceof UploadeFile){
+            $this->updated_at=new DateTime('now');
+        }
+        return $this;
+    }
+    function setImageName($imageName): self {
+        $this->imageName= $imageName;
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+
+        return $this;
+    }
+    /**
+     * @Assert\Callback
+     * @param ExecutionContextInterface $context
+     */
+    public function validate(ExecutionContextInterface $context) {
+        
+        $image= $this->getImageFile();
+        if($image != null && $image !=""){
+            $tailleImage=@getimagesize($image);
+            if(!($tailleImage==false)){
+                if($tailleImage[0]>1300 || $tailleImage[1]>1300){
+                    $context->buildViolation("Cette image est trop grande (1300x1300 max)")
+                            ->atPath('imageFile')
+                            ->addViolation();
+                }
+                else{
+                    $context->buildViolation("Ce n'est pas une image")
+                            ->atPath('imageFile')
+                            ->addViolation();
+                                   
+                }
+            }
+        }
+   
     }
 }
