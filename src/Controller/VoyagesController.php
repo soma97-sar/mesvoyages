0b7c028a9428ae88ec73f60fde16f340dacd2ef2
soma97-sar <BEN BAHA@sokal.lan>
2023-02-28 22:41:31 +0100

    Phase21:sécurité.

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -59,6 +59,7 @@ class VoyagesController extends AbstractController {
      * @return Response
      */
     public function findAllEqual($champ, Request $request):Response {
+        if($this->isCsrfTokenValid('filtre_'.$champ, $request->get('_token'))){
         $valeur=$request->get("recherche");
         $visites=$this->repository->findByEqualValue($champ, $valeur);
         return $this->render("pages/voyages.html.twig",[
@@ -66,6 +67,7 @@ class VoyagesController extends AbstractController {
         ]);
         
     }
+    return $this->redirectToRoute("voyages");
     /**
      * @Route("/voyages/voyage/{id}", name="voyages.showone")
      * @param type $id
--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -7,11 +7,12 @@ use App\Entity\Visite;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\DateTime;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 class VisiteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
@@ -25,7 +26,12 @@ class VisiteType extends AbstractType
                     $options['data']->getDateCreation() !=null ? $options['data']->getDateCreation() : new DateTime('now'),
                 'label'=>'date'
             ])
-            ->add('note')
+            ->add('note', IntegerType::class,[
+                'attr' => [
+                    'min' => 0,
+                    'max' => 20
+                ]
+            ])
             ->add('avis')
             ->add('tempmin',null, [
                 'label'=>'t° min'
